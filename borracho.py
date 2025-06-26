b854c3ae64023c7a800d6c46d2bee8f416fe5ed9
bre3805 <brenda.hermosillo@upnay.edu.mx>
2025-06-26 11:32:24 -0600

    borrachitos

---
+++ borracho.py
@@ -0,0 +1,45 @@
+import time
+
+def tomar(nombre):
+    print(f"{nombre} tomando cerveza...")
+    time.sleep(1)
+    print(f"{nombre} dejo de tomar")
+
+def usar_baño(nombre):
+    print(f"{nombre} orinando...")
+    time.sleep(1)
+    print(f"{nombre} salió del baño.")
+
+def cantar(nombre):
+    print(f"{nombre} cantando...")
+    time.sleep(1)
+    print(f"{nombre} dejo de cantar")
+
+def llamar(nombre):
+    print(f"{nombre} llamando a su ex...")
+    time.sleep(1)
+    print("Llamada rechazada")
+
+def borrachito(nombre1,nombre2,nombre3,nombre4,nombre5):
+    #while True:
+        tomar(nombre1)
+        time.sleep(1)
+        usar_baño(nombre2)
+        time.sleep(1)
+        cantar(nombre3)
+        time.sleep(1)
+        llamar(nombre4)
+        time.sleep(1)
+        tomar(nombre5)
+        time.sleep(1)
+        print("Fin de un ciclo...")
+
+while True:
+    borrachito("Andres", "Jhonat", "Cesar", "Brenda", "Victor")
+    time.sleep(1)
+    borrachito("Cesar", "Brenda", "Jhonat", "Victor", "Andres")
+    time.sleep(1)
+    borrachito("Brenda", "Victor", "Andres", "Jhonat", "Cesar")
+    time.sleep(1)
+    borrachito("Victor", "Andres", "Cesar", "Brenda", "Jhonat")
+    time.sleep(1)
